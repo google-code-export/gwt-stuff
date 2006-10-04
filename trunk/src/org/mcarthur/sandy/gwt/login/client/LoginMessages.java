4851aef42763a4c728ec1c8c713e5ba6735c4fb9
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-10-04 07:49:44 +0000

    Added first version of proper I18N support to the LoginPanel.

---
+++ trunk/src/org/mcarthur/sandy/gwt/login/client/LoginMessages.java
@@ -0,0 +1,14 @@
+package org.mcarthur.sandy.gwt.login.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * Interface for I18N of the Login Panel's messages.
+ *
+ * @author Sandy McArthur
+ */
+public interface LoginMessages extends Messages {
+    public String usernamePrompt();
+    public String passwordPrompt();
+    public String loginButton();
+}
