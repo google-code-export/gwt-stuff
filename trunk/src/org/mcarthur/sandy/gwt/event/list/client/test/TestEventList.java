b6e59210f3afa970414e8aa5b34f0f4fcc02b0f2
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-10-24 22:22:00 +0000

    Added copyrights.

--- trunk/src/org/mcarthur/sandy/gwt/event/list/client/test/TestEventList.java
+++ trunk/src/org/mcarthur/sandy/gwt/event/list/client/test/TestEventList.java
@@ -1,19 +1,34 @@
+/*
+ * Copyright 2006 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.mcarthur.sandy.gwt.event.list.client.test;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import org.mcarthur.sandy.gwt.event.list.client.EventList;
 import org.mcarthur.sandy.gwt.event.list.client.EventLists;
-import org.mcarthur.sandy.gwt.event.list.client.ListEventListener;
 import org.mcarthur.sandy.gwt.event.list.client.ListEvent;
+import org.mcarthur.sandy.gwt.event.list.client.ListEventListener;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * TODO: Write Class JavaDoc
