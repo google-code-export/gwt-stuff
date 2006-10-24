c38766da7c3ab71180f2182dfbd3d4d3907f7bbc
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-10-24 22:19:27 +0000

    Updated Javadocs and copyrights.

--- src/org/mcarthur/sandy/gwt/event/property/client/PropertyChangeEvent.java
+++ src/org/mcarthur/sandy/gwt/event/property/client/PropertyChangeEvent.java
@@ -1,9 +1,37 @@
+/*
+ * Copyright 2006 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.mcarthur.sandy.gwt.event.property.client;
 
 import java.util.EventObject;
 
 /**
  * A "PropertyChange" event gets delivered whenever a "bound" or "constrained" property changes.
+ *
+ * <p>
+ * Normally PropertyChangeEvents are accompanied by the name and the old and new value of the changed property.
+ * If the new value is a primitive type (such as int or boolean) it must be wrapped as the corresponding java.lang.* Object type (such as Integer or Boolean).
+ * </p>
+ * <p>
+ * <code>Null</code> values may be provided for the old and the new values if their true values are not known.
+ * </p>
+ * <p>
+ * An event source may send a <code>null</code> object as the name to indicate that an arbitrary set of if its properties have changed.
+ * In this case the old and new values should also be <code>null</code>.
+ * </p>
  *
  * @see java.beans.PropertyChangeEvent
  */
@@ -13,13 +41,13 @@ public class PropertyChangeEvent extends EventObject {
     private Object newValue;
 
     /**
-     * Constructs a prototypical Event.
+     * Constructs a new PropertyChangeEvent.
      *
      * @param source The object on which fired the event.
-     * @param propertyName The programmatic name of the property that was changed, or null of the whole object changed.
-     * @param oldValue The old value of the property.
+     * @param propertyName The programmatic name of the property that was changed, or <code>null</code> of the whole object changed.
+     * @param oldValue The old value of the property if known.
      * @param newValue The new value of the property.
-     * @throws IllegalArgumentException if source is null.
+     * @throws IllegalArgumentException if source is <code>null</code>.
      */
     public PropertyChangeEvent(final Object source, final String propertyName, final Object oldValue, final Object newValue) {
         super(source);
@@ -28,15 +56,29 @@ public class PropertyChangeEvent extends EventObject {
         this.newValue = newValue;
     }
 
-
+    /**
+     * The programmatic name of the property that was changed.
+     *
+     * @return the programmatic name of the property that was changed or <code>null</code> if the whole object changed  .
+     */
     public String getPropertyName() {
         return propertyName;
     }
 
+    /**
+     * The old value for the property, expressed as an Object.
+     *
+     * @return the old value for the property, expressed as an Object, if known.
+     */
     public Object getOldValue() {
         return oldValue;
     }
 
+    /**
+     * The new value for the property, expressed as an Object.
+     * 
+     * @return the new value for the property, expressed as an Object.
+     */
     public Object getNewValue() {
         return newValue;
     }
--- src/org/mcarthur/sandy/gwt/event/property/client/PropertyChangeListener.java
+++ src/org/mcarthur/sandy/gwt/event/property/client/PropertyChangeListener.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2006 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.mcarthur.sandy.gwt.event.property.client;
 
 /**
@@ -8,5 +24,10 @@ package org.mcarthur.sandy.gwt.event.property.client;
  * @see java.beans.PropertyChangeListener
  */
 public interface PropertyChangeListener {
+    /**
+     * Called when a bound property is changed.
+     *
+     * @param event An event describing the property change.
+     */
     public void propertyChange(PropertyChangeEvent event);
 }
