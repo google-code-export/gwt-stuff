6857884dde13640a2ae25509dcfb88ca711e3a97
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-11-17 03:18:39 +0000

    Moved test code into the test source dir.

--- test/org/mcarthur/sandy/gwt/login/client/TestLoginPanel.java
+++ test/org/mcarthur/sandy/gwt/login/client/TestLoginPanel.java
@@ -1,10 +1,27 @@
-package org.mcarthur.sandy.gwt.login.client.test;
+/*
+ * Copyright 2006 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.mcarthur.sandy.gwt.login.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.Timer;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
 import org.mcarthur.sandy.gwt.login.client.LoginPanel;
+import org.mcarthur.sandy.gwt.login.client.LoginDialogBox;
 
 /**
  * EntryPoint for Testing Login Widgets.
@@ -32,9 +49,24 @@ public class TestLoginPanel implements EntryPoint {
     private final LoginPanel bannerPanel = new LoginPanel(loginListener);
     private final LoginPanel messagePanel = new LoginPanel(loginListener);
 
+    private LoginDialogBox defaultLoginDialogBox;
+
     public void onModuleLoad() {
         final RootPanel defaultLoginPanelRoot = RootPanel.get("defaultLoginPanel");
         defaultLoginPanelRoot.add(defaultPanel);
+
+        final RootPanel defaultLoginDialogBoxRoot = RootPanel.get("defaultLoginDialogBox");
+        defaultLoginDialogBox = new LoginDialogBox(loginListener);
+        Button defaultLoginDialogBoxButton = new Button("Show");
+        defaultLoginDialogBoxButton.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                defaultLoginDialogBox.setPopupPosition(100,100);
+                defaultLoginDialogBox.show();
+                Window.alert("clicked");
+            }
+        });
+        defaultLoginDialogBoxRoot.add(defaultLoginDialogBoxButton);
+
 
 
         final RootPanel bannerLoginPanel = RootPanel.get("bannerLoginPanel");
