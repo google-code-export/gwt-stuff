7dfdd2fddb5bd876b2ea10861f976319c7681b65
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-11-27 23:02:52 +0000

    made class public

--- trunk/src/org/mcarthur/sandy/gwt/table/client/TableBodyGroup.java
+++ trunk/src/org/mcarthur/sandy/gwt/table/client/TableBodyGroup.java
@@ -30,8 +30,8 @@ import com.google.gwt.user.client.DOM;
  * @author Sandy McArthur
  * @see <a href="http://www.w3.org/TR/html4/struct/tables.html#h-11.2.3">HTML Row Group</a>
  */
-abstract class TableBodyGroup extends TableRowGroup {
-    public TableBodyGroup() {
+public abstract class TableBodyGroup extends TableRowGroup {
+    protected TableBodyGroup() {
         super(DOM.createTBody());
         addStyleName("gwtstuff-TableBodyGroup");
     }
