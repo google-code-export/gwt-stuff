c37fbb070cc33ab71407622be91a72893947e791
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2006-12-01 03:36:35 +0000

    Refactored SortedEventList into an interface

--- src/org/mcarthur/sandy/gwt/event/list/client/SortedEventList.java
+++ src/org/mcarthur/sandy/gwt/event/list/client/SortedEventList.java
@@ -16,152 +16,72 @@
 
 package org.mcarthur.sandy.gwt.event.list.client;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 
 /**
- * ALPHA, do not use yet.
  * An EventList that maintains a sorted order of objects add to the list.
- * This List implementation doesn't maintain all of the List contracts.
- * Mainly, any method that adds or sets to a specific index ignores the index parameter so as to
- * maintain sorted order.
+ * This implementation of List does not maintain all of the List contracts.
+ * Mainly, for any method that adds or sets to a specific index the index is ignored to
+ * maintain the elements in sorted order.
  *
  * @author Sandy McArthur
  */
-public class SortedEventList extends WrappedEventList implements EventList {
-    public Comparator comparator; 
+public interface SortedEventList extends EventList {
 
-    private static final Comparator NATURAL = new Comparator() {
+    /**
+     * A Comparator that implements
+     * {@link Comparable natural ordering}.
+     */
+    public static final Comparator NATURAL = new Comparator() {
         public int compare(final Object o1, final Object o2) {
             return ((Comparable)o1).compareTo(o2);
         }
     };
 
-
-    public SortedEventList() {
-        this(NATURAL);
-    }
-
-    public SortedEventList(final Comparator comparator) {
-        super(new ArrayList());
-        this.comparator = comparator;
-    }
-
-    public boolean add(final Object element) {
-        final Comparator comparator = this.comparator != null ? this.comparator : NATURAL;
-
-        int lower = 0;
-        int upper = super.size() - 1;
-
-        while (lower <= upper) {
-            final int middle = (lower + upper) >> 1;
-            final Object o = super.get(middle);
-
-            final int c = comparator.compare(o, element);
-
-            if (c < 0) {
-                lower = middle + 1;
-            } else if (c > 0) {
-                upper = middle - 1;
-            } else {
-                super.add(middle, element);
-                return true;
-            }
-
-        }
-        super.add(lower, element);
-        return true;
-    }
+    /**
+     * Return the Comparator used to sort this list.
+     * If this returns <code>null</code> then this list uses
+     * <a href="http://java.sun.com/j2se/1.5.0/docs/api/java/lang/Comparable.html">natural ordering</a>.
+     *
+     * @return the Comparator used to sort the list, else <code>null</code> if natural ordering is used.
+     */
+    public Comparator getComparator();
 
     /**
-     * Same as {@link #add(Object)}.
+     * Set the Comparator used to sort this list.
+     * If <code>comparator</code> is <code>null</code> then
+     * <a href="http://java.sun.com/j2se/1.5.0/docs/api/java/lang/Comparable.html">natural ordering</a>
+     * is used.
+     * Setting this to a new value causes the list to be resorted.
      *
-     * @param index <em>ignored</em>
+     * @param comparator the Comparator used to sort the list, if <code>null</code> natural ordering is used.
+     */
+    public void setComparator(Comparator comparator);
+
+    /**
+     * Force the list to be resorted.
+     */
+    public void resort();
+
+    /**
+     * The has the same behavior as {@link #add(Object)}.
+     * @param index <em>ignored</em>.
      * @see #add(Object)
      */
-    public void add(final int index, final Object element) {
-        add(element);
-    }
-
-    public boolean addAll(final Collection c) {
-        final Iterator iter = c.iterator();
-        while (iter.hasNext()) {
-            add(iter.next()); // XXX Optimze
-        }
-        return c.size() > 0;
-    }
+    public void add(int index, Object element);
 
     /**
-     * Same as {@link #addAll(java.util.Collection)}.
-     *
-     * @param index <em>ignored</em>
-     * @see #addAll(java.util.Collection)
+     * This has the same behavior as {@link #addAll(Collection)}.
+     * @param index <em>ignored</em>.
+     * @see #addAll(Collection)
      */
-    public boolean addAll(final int index, final Collection c) {
-        return addAll(c);
-    }
+    public boolean addAll(int index, Collection c);
 
     /**
-     * Same as remove(index) and add(Object).
-     *
-     * @param index position of removed element.
-     * @param element object to be added.
-     * @return the removed element.
-     * @see #remove(int) 
-     * @see #add(Object) 
+     * Behaves as if remove(index) and then add(Object) were called.
+     * @see #remove(int)
+     * @see #add(Object)
      */
-    public Object set(final int index, final Object element) {
-        final Object removed = super.remove(index);
-        add(element);
-        return removed;
-    }
-
-    public Comparator getComparator() {
-        return comparator;
-    }
-
-    /**
-     * Set the sort order for this list.
-     *
-     * @param comparator sort order for this list, null for natural ordering.
-     */
-    public void setComparator(final Comparator comparator) {
-        this.comparator = comparator;
-        resort();
-    }
-
-    public void resort() {
-        if (size() == 0) {
-            // nothing to sort
-            return;
-        }
-        final List all = new ArrayList();
-        all.addAll(this);
-        Collections.sort(all, comparator != null ? comparator : NATURAL);
-
-        // XXX: Optimize me
-        if (true) {
-            clear();
-
-        } else {
-            // try to keep runs
-            final Iterator thisIter = this.iterator();
-            final Iterator sortIter = all.iterator();
-            while (sortIter.hasNext()) {
-                final Object thisObj = thisIter.next();
-                final Object sortObj = sortIter.next();
-
-                if (thisObj != sortObj) {
-                    thisIter.remove();
-                } else {
-                    sortIter.remove();
-                }
-            }
-        }
-        addAll(all);
-    }
+    public Object set(int index, Object element);
 }
