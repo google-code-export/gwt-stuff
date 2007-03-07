22952083a5903716fb10fa45e967a9fb8f28e62f
sandymac <sandymac@1ecb6fa1-8b1e-0410-a4ff-f3ae5ebe73c4>
2007-03-07 06:45:21 +0000

    First implementation of the Flash plugin widget. Seems to work well in Firefox and Safari but needs work for IE and Opera.

---
+++ src/org/mcarthur/sandy/gwt/plugin/client/FlashImplIE6.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2007 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.mcarthur.sandy.gwt.plugin.client;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+
+/**
+ * IE6 version of FlashImpl.
+ *
+ * @author Sandy McArthur
+ */
+public class FlashImplIE6 extends FlashImpl {
+
+    public Element createPluginElement() {
+        base = DOM.createElement("object");
+        return base;
+    }
+
+    /**
+     * Do any browser specific initialization here.
+     */
+    public void init() {
+        updateClassId();
+        updateCodeBase();
+    }
+
+    private Element deviceFontParam;
+    public void updateDeviceFont() {
+        if (deviceFontParam == null) {
+            deviceFontParam = createParam("devicefont");
+            DOM.appendChild(base, deviceFontParam);
+        }
+        setAttribute(deviceFontParam, "value", Boolean.toString(flash.isDeviceFont()));
+    }
+
+    private Element movieParam;
+    public void updateMovie() {
+        final String value = flash.getMovie();
+        if (movieParam == null && value != null) {
+            movieParam = createParam("movie");
+            DOM.appendChild(base, movieParam);
+        } else if (movieParam != null && value == null) {
+            DOM.removeChild(base, movieParam);
+            movieParam = null;
+        }
+        if (value != null) {
+            setAttribute(movieParam, "value", value);
+        }
+    }
+
+    public void updateClassId() {
+        setAttribute(base, "classid", flash.getClassId());
+    }
+
+    public void updateCodeBase() {
+        setAttribute(base, "codebase", flash.getCodeBase());
+    }
+
+    public void updatePluginsPage() {
+        // embed only
+    }
+
+    public void updateSwLiveConnect() {
+        // embed only
+    }
+
+    private Element playParam;
+    public void updatePlay() {
+        if (playParam == null) {
+            playParam = createParam("play");
+            DOM.appendChild(base, playParam);
+        }
+        setAttribute(playParam, "value", Boolean.toString(flash.isPlay()));
+    }
+
+    private Element loopParam;
+    public void updateLoop() {
+        if (loopParam == null) {
+            loopParam = createParam("loop");
+            DOM.appendChild(base, loopParam);
+        }
+        setAttribute(loopParam, "value", Boolean.toString(flash.isLoop()));
+    }
+
+    private Element qualityParam;
+    public void updateQuality() {
+        final String value = flash.getQuality();
+        if (qualityParam == null && value != null) {
+            qualityParam = createParam("quality");
+            DOM.appendChild(base, qualityParam);
+        } else if (qualityParam != null && value == null) {
+            DOM.removeChild(base, qualityParam);
+            qualityParam = null;
+        }
+        if (value != null) {
+            setAttribute(qualityParam, "value", value);
+        }
+    }
+
+    private Element bgColorParam;
+    public void updateBgColor() {
+        final String value = flash.getBgColor();
+        if (bgColorParam == null && value != null) {
+            bgColorParam = createParam("bgcolor");
+            DOM.appendChild(base, bgColorParam);
+        } else if (bgColorParam != null && value == null) {
+            DOM.removeChild(base, bgColorParam);
+            bgColorParam = null;
+        }
+        if (value != null) {
+            setAttribute(bgColorParam, "value", value);
+        }
+    }
+
+    private Element scaleParam;
+    public void updateScale() {
+        final String value = flash.getScale();
+        if (scaleParam == null && value != null) {
+            scaleParam = createParam("scale");
+            DOM.appendChild(base, scaleParam);
+        } else if (scaleParam != null && value == null) {
+            DOM.removeChild(base, scaleParam);
+            scaleParam = null;
+        }
+        if (value != null) {
+            setAttribute(scaleParam, "value", value);
+        }
+    }
+
+    private Element salignParam;
+    public void updateSalign() {
+        final String value = flash.getSalign();
+        if (salignParam == null && value != null) {
+            salignParam = createParam("salign");
+            DOM.appendChild(base, salignParam);
+        } else if (salignParam != null && value == null) {
+            DOM.removeChild(base, salignParam);
+            salignParam = null;
+        }
+        if (value != null) {
+            setAttribute(salignParam, "value", value);
+        }
+    }
+
+    private Element baseParam;
+    public void updateBase() {
+        final String value = flash.getBase();
+        if (baseParam == null && value != null) {
+            baseParam = createParam("base");
+            DOM.appendChild(base, baseParam);
+        } else if (baseParam != null && value == null) {
+            DOM.removeChild(base, baseParam);
+            baseParam = null;
+        }
+        if (value != null) {
+            setAttribute(baseParam, "value", value);
+        }
+    }
+
+    private Element menuParam;
+    public void updateMenu() {
+        if (menuParam == null) {
+            menuParam = createParam("menu");
+            DOM.appendChild(base, menuParam);
+        }
+        setAttribute(menuParam, "value", Boolean.toString(flash.isMenu()));
+    }
+
+    private Element wmodeParam;
+    public void updateWmode() {
+        final String value = flash.getWmode();
+        if (wmodeParam == null && value != null) {
+            wmodeParam = createParam("wmode");
+            DOM.appendChild(base, wmodeParam);
+        } else if (wmodeParam != null && value == null) {
+            DOM.removeChild(base, wmodeParam);
+            wmodeParam = null;
+        }
+        if (value != null) {
+            setAttribute(wmodeParam, "value", value);
+        }
+    }
+
+    private Element allowScriptAccessParam;
+    public void updateAllowScriptAccess() {
+        final String value = flash.getAllowScriptAccess();
+        if (allowScriptAccessParam == null && value != null) {
+            allowScriptAccessParam = createParam("allowScriptAccess");
+            DOM.appendChild(base, allowScriptAccessParam);
+        } else if (allowScriptAccessParam != null && value == null) {
+            DOM.removeChild(base, allowScriptAccessParam);
+            allowScriptAccessParam = null;
+        }
+        if (value != null) {
+            setAttribute(allowScriptAccessParam, "value", value);
+        }
+    }
+
+    private Element seamlessTabbingParam;
+    public void updateSeamlessTabbing() {
+        if (seamlessTabbingParam == null) {
+            seamlessTabbingParam = createParam("SeamlessTabbing");
+            DOM.appendChild(base, seamlessTabbingParam);
+        }
+        setAttribute(seamlessTabbingParam, "value", Boolean.toString(flash.isSeamlessTabbing()));
+    }
+
+    private static Element createParam(final String name) {
+        assert name != null;
+        final Element param = DOM.createElement("param");
+        setAttribute(param, "name", name);
+        return param;
+    }
+}
---
+++ test/org/mcarthur/sandy/gwt/plugin/flash/client/TestFlash.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2007 Sandy McArthur, Jr.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.mcarthur.sandy.gwt.plugin.flash.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import org.mcarthur.sandy.gwt.plugin.client.Flash;
+
+/**
+ * Test Flash plugins.
+ *
+ * @author Sandy McArthur
+ */
+public class TestFlash implements EntryPoint {
+    private Flash flash = new Flash("test.swf"); //new Flash("flash_to_javascript.swf");
+    private FlowPanel fp = new FlowPanel();
+    private VerticalPanel log = new VerticalPanel();
+
+    public void onModuleLoad() {
+        //flash.setLoop(true);
+        //flash.setPlay(false);
+        //flash.setSwLiveConnect(true);
+        flash.setWidth("160");
+        flash.setHeight("120");
+        RootPanel.get("flash").add(flash);
+        RootPanel.get("log").add(log);
+
+        flash.addFSCommandListener(new Flash.FsCommandListener() {
+            public void fsCommand(final String command, final String args) {
+                log.insert(new Label("fsCommand: command: " + command + ", args: " + args), 0);
+            }
+        });
+
+        final Button play = new Button("Play");
+        play.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().play();
+            }
+        });
+        fp.add(play);
+
+        final Button rewind = new Button("Rewind");
+        rewind.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().rewind();
+            }
+        });
+        fp.add(rewind);
+
+        final Button stop = new Button("Stop");
+        stop.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().stopPlay();
+            }
+        });
+        fp.add(stop);
+
+        final Button isPlaying = new Button("isPlaying?");
+        isPlaying.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                log.insert(new Label("isPlaying: " + flash.getMethods().isPlaying()), 0);
+            }
+        });
+        fp.add(isPlaying);
+
+        final Button percentLoaded = new Button("Percent Loaded?");
+        percentLoaded.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                log.insert(new Label("Percent Loaded: " + flash.getMethods().percentLoaded()), 0);
+            }
+        });
+        fp.add(percentLoaded);
+
+        final Button totalFrames = new Button("Total Frames?");
+        totalFrames.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                log.insert(new Label("Total Frames: " + flash.getMethods().totalFrames()), 0);
+            }
+        });
+        fp.add(totalFrames);
+
+        final Button zoomIn = new Button("+");
+        zoomIn.setTitle("Zoom In");
+        zoomIn.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().zoom(90);
+            }
+        });
+        fp.add(zoomIn);
+
+        final Button zoomOut = new Button("-");
+        zoomOut.setTitle("Zoom Out");
+        zoomOut.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().zoom(110);
+            }
+        });
+        fp.add(zoomOut);
+
+        final Button resetZoom = new Button("=");
+        resetZoom.setTitle("Reset Zoom");
+        resetZoom.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().zoom(0);
+            }
+        });
+        fp.add(resetZoom);
+
+        final Button panL = new Button("&lt;");
+        panL.setTitle("Pan Left");
+        panL.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().pan(-25, 0, Flash.Methods.PanMode.PIXELS);
+            }
+        });
+        fp.add(panL);
+
+        final Button panU = new Button("/\\");
+        panU.setTitle("Pan Up");
+        panU.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().pan(0, -25, Flash.Methods.PanMode.PIXELS);
+            }
+        });
+        fp.add(panU);
+
+        final Button panR = new Button("&gt;");
+        panR.setTitle("Pan Right");
+        panR.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().pan(25, 0, Flash.Methods.PanMode.PIXELS);
+            }
+        });
+        fp.add(panR);
+
+        final Button panD = new Button("\\/");
+        panD.setTitle("Pan Down");
+        panD.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().pan(0, 25, Flash.Methods.PanMode.PIXELS);
+            }
+        });
+        fp.add(panD);
+
+        final HorizontalPanel hp = new HorizontalPanel();
+        hp.add(new Label("Frame: "));
+        final TextBox frame = new TextBox();
+        hp.add(frame);
+        final Button frameGo = new Button("Go");
+        frameGo.addClickListener(new ClickListener() {
+            public void onClick(final Widget sender) {
+                flash.getMethods().gotoFrame(Integer.parseInt(frame.getText()));
+            }
+        });
+        hp.add(frameGo);
+        fp.add(hp);
+
+        RootPanel.get("buttons").add(fp);
+    }
+}
